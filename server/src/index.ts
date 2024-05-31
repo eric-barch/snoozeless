8802febd836bbf7e66f0946f287f2c10ee9f0e1a
Eric Barch <eric.michael.barch@gmail.com>
2024-05-30 21:56:24 -0400

    feat(server): JavaScript compiles to out directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,17 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import "dotenv/config";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
