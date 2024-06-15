7d5dc3118c226888cefe0898db39ce71c6387374
Eric Barch <eric.michael.barch@gmail.com>
2024-06-14 22:16:02 -0400

    refactor(embedded): Move HTTP utils into own module

---
+++ embedded/main/http_utils/http_utils.h
@@ -0,0 +1,17 @@
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
+
+#include "esp_err.h"
+#include "esp_http_client.h"
+
+#ifndef MAX_HTTP_RX_BUFFER
+#define MAX_HTTP_RX_BUFFER 1024
+#endif
+
+#ifndef MAX_HTTP_TX_BUFFER
+#define MAX_HTTP_TX_BUFFER 1024
+#endif
+
+esp_err_t http_event_handler(esp_http_client_event_t *event);
+
+#endif
