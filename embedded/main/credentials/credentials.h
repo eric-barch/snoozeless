896077be2b095023470dcc2f3f32949b9a7f3680
Eric Barch <eric.michael.barch@gmail.com>
2024-06-18 09:43:12 -0400

    feat(embedded): Implement `initialize_credentials`

---
+++ embedded/main/credentials/credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#include "esp_err.h"
+
+esp_err_t initialize_credentials(void);
+
+#endif
--- embedded/main/main.c
+++ embedded/main/main.c
@@ -1,4 +1,5 @@
 #include "console_utils.h"
+#include "credentials.h"
 #include "freertos/idf_additions.h"
 #include "http_requests.h"
 #include "nvs_utils.h"
@@ -9,6 +10,7 @@ void app_main(void) {
   initialize_console();
 
   initialize_wifi();
+  initialize_credentials();
 
   close_nvs();
   close_console();
