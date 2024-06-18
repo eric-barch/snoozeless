6d46371f31fbca8cebe1daf8977c6a0a662b7c18
Eric Barch <eric.michael.barch@gmail.com>
2024-06-17 21:42:57 -0400

    style(embedded): Format `nvs_utils`

--- embedded/main/nvs_utils/nvs_utils.c
+++ embedded/main/nvs_utils/nvs_utils.c
@@ -4,7 +4,7 @@
 static nvs_handle_t handle;
 
 esp_err_t initialize_nvs(void) {
-    esp_err_t err = nvs_flash_init();
+  esp_err_t err = nvs_flash_init();
 
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -15,58 +15,58 @@ esp_err_t initialize_nvs(void) {
 }
 
 esp_err_t open_nvs_namespace(const char *namespace) {
-    esp_err_t err = nvs_open(namespace, NVS_READWRITE, &handle);
+  esp_err_t err = nvs_open(namespace, NVS_READWRITE, &handle);
 
-    switch (err) {
-    case ESP_OK:
-        printf("Opened NVS namespace: %s\n", namespace);
-        break;
-    default:
-        printf("Error opening NVS namespace: %s\n", esp_err_to_name(err));
-        break;
-    }
+  switch (err) {
+  case ESP_OK:
+    printf("Opened NVS namespace: %s\n", namespace);
+    break;
+  default:
+    printf("Error opening NVS namespace: %s\n", esp_err_to_name(err));
+    break;
+  }
 
-    return err;
+  return err;
 }
 
 esp_err_t get_nvs_str(const char *key, char *out_value, size_t max_length) {
-    esp_err_t err = nvs_get_str(handle, key, out_value, &max_length);
+  esp_err_t err = nvs_get_str(handle, key, out_value, &max_length);
 
-    switch (err) {
-    case ESP_OK:
-        printf("Got string from NVS\n%s: %s\n", key, out_value);
-        break;
-    case ESP_ERR_NVS_NOT_FOUND:
-        printf("Key not found in NVS: %s\n", key);
-        break;
-    default:
-        printf("Error getting string from NVS: %s\n", esp_err_to_name(err));
-        break;
-    }
+  switch (err) {
+  case ESP_OK:
+    printf("Got string from NVS\n%s: %s\n", key, out_value);
+    break;
+  case ESP_ERR_NVS_NOT_FOUND:
+    printf("Key not found in NVS: %s\n", key);
+    break;
+  default:
+    printf("Error getting string from NVS: %s\n", esp_err_to_name(err));
+    break;
+  }
 
-    return err;
+  return err;
 }
 
 esp_err_t set_nvs_str(const char *key, const char *in_value) {
-    esp_err_t err = nvs_set_str(handle, key, in_value);
+  esp_err_t err = nvs_set_str(handle, key, in_value);
 
-    if (err == ESP_OK) {
-        err = nvs_commit(handle);
-    }
+  if (err == ESP_OK) {
+    err = nvs_commit(handle);
+  }
 
-    switch (err) {
-    case ESP_OK:
-        printf("Set string in NVS\n%s: %s\n", key, in_value);
-        break;
-    default:
-        printf("Error setting string in NVS: %s\n", esp_err_to_name(err));
-        break;
-    }
+  switch (err) {
+  case ESP_OK:
+    printf("Set string in NVS\n%s: %s\n", key, in_value);
+    break;
+  default:
+    printf("Error setting string in NVS: %s\n", esp_err_to_name(err));
+    break;
+  }
 
-    return err;
+  return err;
 }
 
 void close_nvs(void) {
-    nvs_close(handle);
-    printf("Closed NVS handle.\n");
+  nvs_close(handle);
+  printf("Closed NVS handle.\n");
 }
