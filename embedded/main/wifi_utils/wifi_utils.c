46f2cfe0849dfdb51db45c8136278d888209fe1e
Eric Barch <eric.michael.barch@gmail.com>
2024-06-17 21:38:10 -0400

    style(embedded): Format `wifi_utils`

--- embedded/main/wifi_utils/wifi_utils.c
+++ embedded/main/wifi_utils/wifi_utils.c
@@ -1,10 +1,11 @@
+#include "wifi_utils.h"
+#include "console_utils.h"
 #include "esp_err.h"
 #include "esp_event_base.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "freertos/idf_additions.h"
 #include "nvs_utils.h"
-#include "wifi_utils.h"
 #include <string.h>
 
 #define WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
@@ -13,15 +14,12 @@
 #define WIFI_MAX_PASSWORD_LENGTH 64
 #define WIFI_MAX_RETRY 5
 
-/**s_wifi_event_group allows multiple bits for each event, but we
- * only care about two: (1) we've connected to the AP with an IP, or (2) we've
- * failed to connect after the maximum number of retries. */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 
 typedef struct {
-    char ssid[WIFI_MAX_SSID_LENGTH];
-    char password[WIFI_MAX_PASSWORD_LENGTH];
+  char ssid[WIFI_MAX_SSID_LENGTH];
+  char password[WIFI_MAX_PASSWORD_LENGTH];
 } wifi_credentials_t;
 
 static char *TAG = "wifi_utils";
@@ -92,20 +90,13 @@ void wifi_event_handler(void *arg, esp_event_base_t event_base,
   }
 }
 
-void connect_to_wifi(void) {
-    s_wifi_event_group = xEventGroupCreate();
+esp_err_t connect_to_wifi(void) {
+  s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
 
     ESP_ERROR_CHECK(esp_event_handler_instance_register(
         WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL,
@@ -114,26 +105,9 @@ void connect_to_wifi(void) {
         IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL,
         &instance_got_ip));
 
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "",
-            .password = "",
-            .threshold.authmode = WIFI_SCAN_AUTH_MODE_THRESHOLD,
-            .sae_pwe_h2e = WIFI_SAE_MODE,
-        },
-    };
-
-    strncpy((char *)wifi_config.sta.ssid, wifi_credentials.ssid, sizeof(wifi_config.sta.ssid) - 1);
-    strncpy((char *)wifi_config.sta.password, wifi_credentials.password,
-            sizeof(wifi_config.sta.password) - 1);
-
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
-
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-                                           pdFALSE, pdFALSE, portMAX_DELAY);
 
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "Connected to SSID %s with password %s", wifi_credentials.ssid, wifi_credentials.password);
@@ -142,6 +116,32 @@ void connect_to_wifi(void) {
     } else {
         ESP_LOGE(TAG, "Unexpected event connecting to SSID %s with password %s", wifi_credentials.ssid, wifi_credentials.password);
     }
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = "",
+              .password = "",
+              .threshold.authmode = WIFI_SCAN_AUTH_MODE_THRESHOLD,
+              .sae_pwe_h2e = WIFI_SAE_MODE,
+          },
+  };
+
+  strncpy((char *)wifi_config.sta.ssid, wifi_credentials.ssid,
+          sizeof(wifi_config.sta.ssid) - 1);
+  strncpy((char *)wifi_config.sta.password, wifi_credentials.password,
+          sizeof(wifi_config.sta.password) - 1);
+
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
+
 }
 
 void initialize_wifi(void) {
