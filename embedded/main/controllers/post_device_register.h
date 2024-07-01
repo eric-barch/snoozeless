77f2d6a7035fbda5ea3692a86947820706cf2bea
Eric Barch <eric.michael.barch@gmail.com>
2024-06-30 20:13:32 -0400

    refactor(embedded): Clear working directory for C++ rewrite

--- embedded/main/controllers/post_device_register.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLLERS_POST_DEVICE_REGISTER_H
-#define CONTROLLERS_POST_DEVICE_REGISTER_H
-
-#include "esp_err.h"
-#include "state/device.h"
-
-esp_err_t post_device_register(device_state_t *device);
-
-#endif
--- embedded/main/services/real_time.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVICES_REAL_TIME_H
-#define SERVICES_REAL_TIME_H
-
-void calibrate_real_time_task(void *pvParameters);
-
-#endif
