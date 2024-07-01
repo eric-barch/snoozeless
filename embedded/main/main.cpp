7b4bccf4f1ff4718fe5a4fb5ddf9c47254458643
Eric Barch <eric.michael.barch@gmail.com>
2024-06-30 20:25:11 -0400

    feat(embedded): App builds with main.cpp

---
+++ embedded/main/main.cpp
@@ -0,0 +1,16 @@
+extern "C" void app_main(void) {
+  // NvsManager nvs_manager;
+  //
+  // WifiManager wifi_manager(nvs_manager);
+  // User user(nvs_manager);
+  // Device device(nvs_manager, user);
+  //
+  // CurrentTime current_time(nvs_manager, user, device);
+  // Display display(nvs_manager, current_time);
+  //
+  // user.maintain_session();
+  // device.subscribe();
+  // current_time.subscribe();
+  //
+  // display.show_current_time();
+}
