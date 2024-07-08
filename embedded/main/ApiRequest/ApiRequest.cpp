e578034500e009d6fcdd8954bb228f6d39afe6cf
Eric Barch <eric.michael.barch@gmail.com>
2024-07-08 14:30:48 -0400

    feat(embedded): Callback memory address is preserved when passed to `send_request_task`

--- embedded/main/ApiRequest/ApiRequest.cpp
+++ embedded/main/ApiRequest/ApiRequest.cpp
@@ -5,6 +5,7 @@
 #include "esp_http_client.h"
 #include "esp_log.h"
 #include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include <string>
 
@@ -109,60 +110,21 @@ esp_err_t ApiRequest::http_event_handler(esp_http_client_event_t *event) {
 void ApiRequest::send_request_task(void *pvParameters) {
   ApiRequest *api_request = static_cast<ApiRequest *>(pvParameters);
 
-  esp_http_client_config_t config = {
-      .host = "192.168.1.6",
-      .port = 3000,
-      .path = api_request->path.c_str(),
-      .query = api_request->query.c_str(),
-      .method = api_request->method,
-      .timeout_ms = api_request->timeout_ms,
-      .event_handler = &ApiRequest::http_event_handler,
-      .transport_type = HTTP_TRANSPORT_OVER_SSL,
-      .buffer_size = MAX_HTTP_RX_BUFFER,
-      .buffer_size_tx = MAX_HTTP_TX_BUFFER,
-      .user_data = api_request,
-      .is_async = true,
-      .crt_bundle_attach = esp_crt_bundle_attach,
-  };
+  void *caller_context = api_request->caller_context;
+  OnDataCallback on_data = api_request->on_data;
 
-  esp_http_client_handle_t client = esp_http_client_init(&config);
-
-  std::string auth_bearer_header =
-      "Bearer " + api_request->session.get_auth_bearer_token();
-  ESP_LOGD(TAG, "auth_bearer_header: %s", auth_bearer_header.c_str());
-  esp_http_client_set_header(client, "Authorization",
-                             auth_bearer_header.c_str());
-
-  std::string refresh_token_header = api_request->session.get_refresh_token();
-  ESP_LOGD(TAG, "refresh_token_header: %s", refresh_token_header.c_str());
-  esp_http_client_set_header(client, "Refresh-Token",
-                             refresh_token_header.c_str());
-
-  esp_err_t err;
-
-  while (true) {
-    err = esp_http_client_perform(client);
-    if (err != ESP_ERR_HTTP_EAGAIN) {
-      break;
-    }
-    vTaskDelay(pdMS_TO_TICKS(1000));
-  };
-
-  esp_http_client_close(client);
-  esp_http_client_cleanup(client);
+  std::string simulated_response = "Data from your path!";
+  if (on_data) {
+    on_data(caller_context, simulated_response);
+  }
 
   vTaskDelete(NULL);
 }
 
 void ApiRequest::send_request() {
-  // xTaskCreate(&ApiRequest::send_request_task, "send_request_task", 8192,
-  // this,
-  //             5, NULL);
-
-  ESP_LOGI(TAG, "Caller memory address in send_request: %p", caller_context);
-  ESP_LOGI(TAG, "on_data memory address in send_request: %p", on_data);
-  std::string simulated_response = "Data from " + path;
-  if (on_data) {
-    on_data(caller_context, simulated_response);
+  BaseType_t err = xTaskCreate(&ApiRequest::send_request_task,
+                               "send_request_task", 8192, this, 5, NULL);
+  if (err != pdPASS) {
+    ESP_LOGE(TAG, "Failed to create send_request_task.");
   }
 }
