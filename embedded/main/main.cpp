08a5140d0b6a3199d000adfed0e575599017dbe5
Eric Barch <eric.michael.barch@gmail.com>
2024-07-16 19:48:46 -0400

    style(embedded): Remove extraneous comments from `main`

--- embedded/main/main.cpp
+++ embedded/main/main.cpp
@@ -11,10 +11,6 @@ static const char *TAG = "main";
 extern "C" void app_main(void) {
   NvsManager nvs_manager;
 
-  // nvs_manager.erase_key("session", "access");
-  // nvs_manager.erase_key("session", "refresh");
-  // nvs_manager.erase_key("device", "id");
-
   WifiManager wifi_manager(nvs_manager);
   Session session(nvs_manager);
   CurrentTime current_time(nvs_manager, session);
