e634b7f36d1e225c03e124878cd119437ad623e4
Eric Barch <eric.michael.barch@gmail.com>
2024-07-26 14:50:30 -0400

    feat(embedded): Implement `Alarms.parse_alarm_insert()`

--- embedded/main/Alarms/Alarms.cpp
+++ embedded/main/Alarms/Alarms.cpp
@@ -51,7 +51,7 @@ Alarms::Alarms(NvsManager &nvs_manager) : nvs_manager(nvs_manager) {
 void Alarms::parse_initial_alarms(const std::string &data) {
   cJSON *alarms_json = cJSON_Parse(data.c_str());
   if (!alarms_json) {
-    ESP_LOGE(TAG, "Error parsing JSON data.");
+    ESP_LOGE(TAG, "Error parsing JSON alarms.");
     return;
   }
 
@@ -64,7 +64,7 @@ void Alarms::parse_initial_alarms(const std::string &data) {
   std::map<std::string, std::unique_ptr<Alarm>> alarms;
   const cJSON *alarm_json = nullptr;
   cJSON_ArrayForEach(alarm_json, alarms_json) {
-    const cJSON *id_json = cJSON_GetObjectItem(alarm_json, "id");
+    const cJSON *const id_json = cJSON_GetObjectItem(alarm_json, "id");
     if (!cJSON_IsString(id_json) || (id_json->valuestring == nullptr)) {
       ESP_LOGE(TAG, "Alarm does not have a valid id.");
       continue;
@@ -77,7 +77,6 @@ void Alarms::parse_initial_alarms(const std::string &data) {
 
   this->alarms = std::move(alarms);
 
-  // Step 5: Create a new cJSON array and add each alarm ID to it
   cJSON *alarm_ids_array = cJSON_CreateArray();
   for (const auto &pair : this->alarms) {
     cJSON_AddItemToArray(alarm_ids_array,
@@ -102,7 +101,50 @@ void Alarms::parse_initial_alarms(const std::string &data) {
 }
 
 void Alarms::parse_alarm_insert(const std::string &data) {
-  ESP_LOGW(TAG, "Implement alarm insert.");
+  cJSON *const alarm_json = cJSON_Parse(data.c_str());
+  if (!alarm_json) {
+    ESP_LOGE(TAG, "Error parsing JSON alarm.");
+    return;
+  }
+
+  if (!cJSON_IsObject(alarm_json)) {
+    ESP_LOGE(TAG, "Alarm is not an object.");
+    cJSON_Delete(alarm_json);
+    return;
+  }
+
+  const cJSON *const id_json = cJSON_GetObjectItem(alarm_json, "id");
+  if (!cJSON_IsString(id_json) || (id_json->valuestring == nullptr)) {
+    ESP_LOGE(TAG, "Alarm does not have a valid id.");
+    cJSON_Delete(alarm_json);
+    return;
+  }
+
+  std::string id = id_json->valuestring;
+  auto new_alarm = std::make_unique<Alarm>(this->nvs_manager, alarm_json);
+  alarms[id] = std::move(new_alarm);
+
+  cJSON *alarm_ids_array = cJSON_CreateArray();
+  for (const auto &pair : this->alarms) {
+    cJSON_AddItemToArray(alarm_ids_array,
+                         cJSON_CreateString(pair.first.c_str()));
+  }
+
+  char *alarm_ids_string = cJSON_PrintUnformatted(alarm_ids_array);
+
+  if (alarm_ids_string) {
+    esp_err_t err =
+        this->nvs_manager.write_string("alarms", "ids", alarm_ids_string);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Alarm IDs written to NVS: %s", alarm_ids_string);
+    } else {
+      ESP_LOGE(TAG, "Error writing alarm IDs to NVS: %s", esp_err_to_name(err));
+    }
+    free(alarm_ids_string);
+  }
+
+  cJSON_Delete(alarm_json);
+  cJSON_Delete(alarm_ids_array);
 };
 
 void Alarms::parse_alarm_update(const std::string &data) {
