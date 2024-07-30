e5acaf8e36ebca577216213263b5181cc5cb2be1
Eric Barch <eric.michael.barch@gmail.com>
2024-07-30 12:57:57 -0400

    fix(embedded): Fix `Display` crash on initial startup

--- embedded/main/Display/Display.cpp
+++ embedded/main/Display/Display.cpp
@@ -43,8 +43,6 @@ Display::Display(NonVolatileStorage &non_volatile_storage,
   io_conf.intr_type = GPIO_INTR_DISABLE;
   io_conf.mode = GPIO_MODE_OUTPUT;
   io_conf.pin_bit_mask = (1ULL << GPIO_NUM_2);
-  io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-  io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
   gpio_config(&io_conf);
   gpio_set_level(GPIO_NUM_2, 1);
 
