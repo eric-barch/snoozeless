dc62f8efae7e20ecb6864d7dd172441eadb3df85
Eric Barch <eric.michael.barch@gmail.com>
2024-07-30 20:45:03 -0400

    style(embedded): Organize `main`

--- embedded/main/main.cpp
+++ embedded/main/main.cpp
@@ -15,15 +15,20 @@
  *   class instead of handling all that logic inside `Device`. */
 
 extern "C" void app_main(void) {
+  /**Utilities. */
   NonVolatileStorage non_volatile_storage;
   WifiConnection wifi_connection(non_volatile_storage);
 
+  /**State. */
   Session session(non_volatile_storage);
   CurrentTime current_time(non_volatile_storage, session);
   Alarms alarms(non_volatile_storage);
+
+  /**Peripherals. */
   Display display(non_volatile_storage, current_time);
   Buzzer buzzer;
 
+  /**Central coordinator. */
   Device device(non_volatile_storage, session, current_time, alarms, display,
                 buzzer);
 
