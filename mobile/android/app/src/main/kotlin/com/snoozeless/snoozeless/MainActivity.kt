5586407a4666eddae9ab428cc12d85722256a30e
Eric Barch <eric.michael.barch@gmail.com>
2025-02-08 12:01:32 -0500

    chore: Clear repo for rewrite

--- mobile/android/app/src/main/kotlin/com/snoozeless/snoozeless/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.snoozeless.snoozeless
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- server/src/controllers/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "hono";
-import { refreshAuthService } from "@/services/auth";
-
-export const refreshAuthController = async (c: Context): Promise<Response> => {
-  const response = await refreshAuthService(c);
-  return c.json(response, 201);
-};
